34880e9ee2e786f831e8306353178aab877f9ce4
Weilun Chiu <weilun.chiu@loves.com>
2026-05-13 12:26:50 -0500

    added two staging models, re-factored dim_customers, and establish materilization strategy

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,29 +1,12 @@
-{{ config(
-    materialized='table'
-)
-}}
-
-
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from {{ ref('raw_customers') }}
+    select * from {{ ref('stg_jaffle_shop_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from {{ ref('raw_orders') }}
+    select * from {{ ref('stg_jaffle_shop_orders') }}
 
 ),
 
